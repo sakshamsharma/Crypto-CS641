38ea60ee3d1607f825455619a7e89df809fe2523
Saksham Sharma <saksham0808@gmail.com>
2017-02-18 22:10:32 +0530

    Partially write DES breaking code

--- get_bits_from_output.py
+++ get_bits_from_output.py
@@ -13,13 +13,35 @@ if len(sys.argv) < 2:
 def getbytestrings(inbytes):
   return ("{0:020b}".format(int(inbytes, 16))).zfill(64)
 
+def splitInHalf(st):
+  return st[:len(st)//2], st[len(st)//2:]
+
+def toBitList(st):
+  return list(map(lambda x: ord(x)-ord('0'), st))
+
 with open(sys.argv[1]) as f:
   for line in f:
     line = line.rstrip().split(' ')
     line[0] = line[0].translate(trantab).replace(':', '')
     line[1] = line[1].translate(trantab).replace(':', '')
-    in1, in2 = line[0][:len(line[0])//2], line[0][len(line[0])//2:]
-    out1, out2 = line[1][:len(line[1])//2], line[1][len(line[1])//2:]
 
-    # print(in1, in2, out1, out2)
-    print(getbytestrings(in1))
+    in0, in1 = splitInHalf(line[0])
+    out0, out1 = splitInHalf(line[1])
+
+    pout0 = ''.join(util.applyBitPermutation(util.rfpinv, list(getbytestrings(out0))))
+    pout1 = ''.join(util.applyBitPermutation(util.rfpinv, list(getbytestrings(out1))))
+
+    def helper(k):
+      a, b = k
+      return toBitList(a), toBitList(b)
+
+    lf0, rf0 = helper(splitInHalf(pout0))
+    lf1, rf1 = helper(splitInHalf(pout1))
+
+    cons = toBitList(''.join(map(util.byteToBitString, util.xor))[:32])
+
+    rPrime = util.xorBitList(rf0, rf1)
+    cPrime = util.applyBitPermutation(util.pinv, util.xorBitList(rPrime, cons))
+
+    e0 = util.applyBitPermutation(util.exp, lf0)
+    e1 = util.applyBitPermutation(util.exp, lf1)
