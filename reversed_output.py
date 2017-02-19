c017cf83a665c476c7efffda158a37acebc7b99a
Saksham Sharma <saksham0808@gmail.com>
2017-02-19 18:21:38 +0530

    Add text files to git

--- reversed_output.py
+++
@@ -1,6 +0,0 @@
-inverted = 'lklgmolnlnmplplrlsmpifififififif'
-nums =[101, 97, 121, 104, 104, 122, 106, 108, 109, 122, 48, 48, 48, 48, 48, 48]
-
-for num in nums:
-  print(chr(num), end='')
-print()
