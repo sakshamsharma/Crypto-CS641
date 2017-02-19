589a12f2ce3f00c836cabcdf703b72fad125ed8b
Saksham Sharma <saksham0808@gmail.com>
2017-02-19 23:49:54 +0530

    Remove useless script

--- convert_final_output_to_password.py
+++
@@ -1,8 +0,0 @@
-intab = "0123456789abcdef"
-outtab = "fghijklmnopqrstu"
-trantab = str.maketrans(outtab, intab)
-
-final_out = 'lklgmolnlnmplplrlsmlifififififif'
-final_out = final_out.translate(trantab)
-print(final_out)
-print(bytearray.fromhex(final_out).decode())
